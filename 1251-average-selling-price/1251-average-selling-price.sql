5ce4497dcc6dc6cb2e546f6abac442af5d2eb26f
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-10-02 00:00:10 +0530

    10-02-2025 1251-average-selling-price .sql

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,5 +1,1 @@
-# Write your MySQL query statement below
-select p.product_id,IFNULL(ROUND(sum(p.price*u.units)/sum(u.units),2),0) as average_price from Prices p left join UnitsSold u
-ON p.product_id=u.product_id
-and u.purchase_date between p.start_date and p.end_date
-group by p.product_id;
+select p.product_id,ifnull(round((sum(p.price*u.units)/sum(u.units)),2),0) as average_prie from Prices p left join UnitsSold u on p.product_id=u.product_id and u.purchase_date between p.start_date and p.end_date gorup by p.product_id;
