084e76e74585086f426f3b17ecf874d8b7d80808
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-10-01 22:48:03 +0530

    10-01-2025 1518-water-bottles .java

--- 1518-water-bottles/1518-water-bottles.java
+++ 1518-water-bottles/1518-water-bottles.java
@@ -1,12 +1,11 @@
 class Solution {
-    public int numWaterBottles(int nb, int ne) {
-       int total=nb;
-        while(nb>=ne)
-        {
-         int drunk=(nb/ne);
-         int rem=nb%ne;
-         total+=drunk;
-         nb=drunk+rem;
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int total=numBottles;
+        while(numBottles>=numExchange){
+            int drunk=(numBottles/numExchange);
+            int rem=(numBottles%numExchange);
+            total+=drunk;
+            numBottles=rem+drunk;
         }
         return total;
     }
