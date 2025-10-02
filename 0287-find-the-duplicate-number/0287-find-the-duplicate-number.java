7ccfab521d937994509911cff6dbce52a6960dc9
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-10-02 14:51:10 +0530

    10-02-2025 0287-find-the-duplicate-number .java

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,25 +1,25 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-    int i=0;
-    while(i<nums.length){
-        if(nums[i]<i+1){
+        int i=0;
+        while(i<nums.length){
             int correct=nums[i]-1;
-            if(nums[i]!=nums[correct]){
+            if(nums[i]<i+1){
+                if(nums[i]!=nums[correct]){
                 swap(nums,i,correct);
             }
             else{
                 return nums[i];
             }
+            }
+            else{
+                i++;
+            }
         }
-        else{
-            i++;
-        }
+        return nums.length;
     }
-    return -1;
-    }
-    public void swap(int[] arr,int f,int s){
-        int temp=arr[f];
-        arr[f]=arr[s];
-        arr[s]=temp;
+    public void swap(int [] arr,int from,int to){
+        int temp=arr[from];
+        arr[from]=arr[to];
+        arr[to]=temp;
     }
 }
