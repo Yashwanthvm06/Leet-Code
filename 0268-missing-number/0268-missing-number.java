d53fb541b078f9147ab9104854423b64a8fd751e
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-10-02 15:04:36 +0530

    10-02-2025 0268-missing-number .java

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,24 +2,26 @@ class Solution {
     public int missingNumber(int[] nums) {
         int i=0;
         while(i<nums.length){
-            int current=nums[i];
-            if(nums[i]<nums.length && nums[i]!=nums[current]){
-                swap(nums,i,current);
+            int correct_pos=nums[i];
+            if(nums[i]<nums.length && nums[i]!=nums[correct_pos]){
+                swap(nums,i,correct_pos);
             }
             else{
                 i++;
             }
         }
-        for(int j=0;j<nums.length;j++){
-            if(nums[j]!=j){
-                return j;
+        for(int miss=0;miss<nums.length;miss++)
+        {
+            if(nums[miss]!=miss)
+            {
+                return miss;
             }
         }
-return nums.length;
+        return nums.length;
     }
-    public void swap(int [] arr,int i,int current){
-        int temp=arr[i];
-        arr[i]=arr[current];
-        arr[current]=temp;
+    public void swap(int[] arr,int from ,int where){
+        int temp=arr[from];
+        arr[from]=arr[where];
+        arr[where]=temp;
     }
 }
