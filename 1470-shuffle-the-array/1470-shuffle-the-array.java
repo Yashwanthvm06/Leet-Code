4d6aecd616c58014abe347ff3037d3f0cd11c46e
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-10-04 21:28:05 +0530

    10-04-2025 1470-shuffle-the-array .java44.9 MB (26.11%) 0 ms (100%) .java

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int [] temp=new int[nums.length];
-     for(int i=0;i<n;i++)   
-     {
-        temp[2*i]=nums[i];
-        temp[2*i+1]=nums[n+i];
-     }
-     return temp;
+        int [] temp=new int [nums.length];
+        for(int i=0;i<n;i++){
+            temp[2*i]=nums[i];
+            temp[2*i+1]=nums[i+n];
+        }
+        return temp;
     }
 }
