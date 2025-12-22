9858baf200fdff7e24fe2d5c8727c19eeb4fd8d8
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-12-22 18:29:29 +0530

    12-22-2025 47.5 MB (40.72%) 1 ms (99.99%) 0977-squares-of-a-sorted-array

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,11 +1,23 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int l=0;
-        int r=nums.length-1;
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
+        int n=nums.length;
+        int[] result=new int[n];
+        int r=n-1;
+        int index=n-1;
+        while(l<=r){
+            int left=nums[l]*nums[l];
+            int right=nums[r]*nums[r];
+            if(left>right){
+                result[index--]=left;
+                l++;
+            }
+            else{
+                result[index--]=right;
+                r--;
+            }
         }
-        Arrays.sort(nums);
-        return nums;
+    return result;
     }
+
 }
