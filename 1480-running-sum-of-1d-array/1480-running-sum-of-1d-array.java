1c09828c7372f77450cc08501f3a90532e337218
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2025-12-26 18:07:12 +0530

    12-26-2025 43.2 MB (93.51%) 0 ms (100%) 1480-running-sum-of-1d-array

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        int check=0;
-        int[] sum=new int[nums.length];
-        for(int i=0;i<nums.length;i++)
-        {
-check+=nums[i];
-sum[i]=check;
+        for(int i=1;i<nums.length;i++){
+            nums[i]=nums[i]+nums[i-1];
         }
-    return sum;
+        return nums;
     }
 }
