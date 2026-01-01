3555fb5f11e6fc67788c3bbf54abc01ba22e20ed
Yashwanthvm@06 <yashwanthvm06@gmail.com>
2026-01-02 00:56:11 +0530

    01-02-2026 43.6 MB (33.83%) 0 ms (100%) 0019-remove-nth-node-from-end-of-list

---
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = new ListNode(0,head);
+        ListNode first=temp;
+        ListNode willdelete=temp;
+        for(int i=0;i<=n;i++){
+            first=first.next;   
+        }
+        while(first != null){
+            first=first.next;
+            willdelete=willdelete.next;
+        }
+        willdelete.next=willdelete.next.next;
+        return temp.next;
+    }
+}
